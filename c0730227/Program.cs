bab8274aac67c5a145edbd373c0ab80f250885e3
prabhjot730227 <prabhjot7brar@gmail.com>
2019-02-07 14:29:56 -0500

    205 PM COMMIT

--- c0730227/Program.cs
+++ c0730227/Program.cs
@@ -10,6 +10,31 @@ namespace c0730227
     {
         static void Main(string[] args)
         {
+            var a = new TestQuestion2();
+            a.PlayingWithForLoops();
+        }
+    }
+
+    class TestQuestion2
+    {
+        public int myFavouriteVariable = 0;
+        public void PlayingWithForLoops()
+        {
+            for (; MyMethod();)
+            {
+                if (myFavouriteVariable > 10)
+                {
+                    Console.WriteLine("I am so out of here!");
+                    break;
+                }
+                Console.WriteLine("Oh no I have to go through this stupid loop again...");
+            }
+
+        }
+        public bool MyMethod()
+        {
+            myFavouriteVariable++;
+            return true;
         }
     }
 }
